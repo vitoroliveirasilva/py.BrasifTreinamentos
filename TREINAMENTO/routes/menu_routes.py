82e29ecfac72dee5b66433d7374fa039cf193fb0
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-04 15:00:00 -0300

    FEAT - Criação de menu principal sem estilização

--- TREINAMENTO/routes/menu_routes.py
+++ TREINAMENTO/routes/menu_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 menu_bp = Blueprint("menu", __name__)
@@ -7,4 +7,4 @@ menu_bp = Blueprint("menu", __name__)
 @menu_bp.route("/", methods=["GET", "POST"])
 @login_required
 def menu():
-    return render_template("/index.html")
+    return render_template("/index.html", nome_responsavel=current_user.nome, id_responsavel=current_user.id)

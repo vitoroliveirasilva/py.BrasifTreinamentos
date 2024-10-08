1e7e2607a5bd5ddeb7192aafea178e426bcfac59
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-08 10:20:08 -0300

    FIX - Validação Responsavel

--- TREINAMENTO/routes/responsavel/edicao_responsavel_routes.py
+++ TREINAMENTO/routes/responsavel/edicao_responsavel_routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_required
 from TREINAMENTO import db
+from sqlalchemy.exc import SQLAlchemyError
 from TREINAMENTO.forms.responsavel_forms import ResponsavelForm
 from TREINAMENTO.models import Responsavel
 from . import responsavel_bp
@@ -11,11 +12,25 @@ from . import responsavel_bp
 def editar_responsavel(id):
     responsavel = Responsavel.query.get_or_404(id)
     form = ResponsavelForm(obj=responsavel)
+    
 
     if form.validate_on_submit():
-        responsavel.atualizar_responsavel(form)
-        db.session.commit()
-        flash("Responsável atualizado com sucesso!", "success")
-        return redirect(url_for("responsavel.cadastro_empresa"))
+        try:
+            responsavel.atualizar_responsavel(form)
+            db.session.commit()
+            flash("Responsável atualizado com sucesso!", "success")
+            return redirect(url_for("responsavel.cadastro_empresa"))
+        
+        except ValueError as ve:
+            db.session.rollback()
+            flash(str(ve), "warning")
+
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            flash(f"Erro ao acessar o banco de dados: {str(e)}", "danger")
+
+        except Exception as e:
+            db.session.rollback()
+            flash(f"Erro inesperado: {str(e)}", "danger")
 
     return render_template("/edicao/edicao_responsavel.html", form=form, responsavel=responsavel)

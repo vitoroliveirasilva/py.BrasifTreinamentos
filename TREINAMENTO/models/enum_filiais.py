d81c9e63988f4914035ef5a7026956ad96d9bfd7
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-08 22:43:49 -0300

    FIX - Acentuação em Enum Filiais

--- TREINAMENTO/models/enum_filiais.py
+++ TREINAMENTO/models/enum_filiais.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class Filiais(Enum):
-    JUNDIAI = "Jundiaí"
+    JUNDIAÍ = "Jundiaí"
     BELO_HORIZONTE = "Belo Horizonte"
-    RIBEIRAO_PRETO = "Ribeirão Preto"
-    CUIABA = "Cuiabá"
+    RIBEIRÃO_PRETO = "Ribeirão Preto"
+    CUIABÁ = "Cuiabá"
     RIO_DE_JANEIRO = "Rio de Janeiro"
     TOCANTINS = "Tocantins"
-    BRASILIA = "Brasília"
-    GOIANIA = "Goiânia"
+    BRASÍLIA = "Brasília"
+    GOIÂNIA = "Goiânia"
     CURITIBA = "Curitiba"

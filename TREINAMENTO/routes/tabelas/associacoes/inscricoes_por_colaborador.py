9ff4e1fbeeece900ce22526822d55b428e62af43
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-10 15:15:31 -0300

    FIX - Inscrições por colaborador

--- TREINAMENTO/routes/tabelas/associacoes/inscricoes_por_colaborador.py
+++ TREINAMENTO/routes/tabelas/associacoes/inscricoes_por_colaborador.py
@@ -1,23 +1,20 @@
 from flask import render_template, request
-from TREINAMENTO import db
 from flask_login import login_required
-from TREINAMENTO.models import Inscricao, Treinamento, Colaborador, Marca, Responsavel
+from TREINAMENTO.models import Inscricao, Colaborador
 from .. import tabela_bp
+
 
 @tabela_bp.route('/inscricoes/colaborador/<int:id_colaborador>')
 @login_required
 def inscricoes_por_colaborador(id_colaborador):
+    # Busca o colaborador pelo ID, ou retorna 404 se não encontrar
+    colaborador = Colaborador.query.get_or_404(id_colaborador)
+
+    # Parâmetros de paginação
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
 
-    # Consulta para trazer as inscrições de um colaborador específico com o responsável
-    inscricoes = db.session.query(Inscricao, Treinamento, Marca, Responsavel)\
-        .join(Treinamento, Inscricao.id_treinamento == Treinamento.id_treinamento)\
-        .join(Marca, Treinamento.id_marca == Marca.id_marca)\
-        .join(Responsavel, Inscricao.id_responsavel == Responsavel.id)\
-        .filter(Inscricao.id_colaborador == id_colaborador)\
-        .paginate(page=page, per_page=per_page)
+    # Consulta para trazer todas as inscrições por colaborador
+    inscricoes = Inscricao.query.filter_by(id_colaborador=id_colaborador).paginate(page=page, per_page=per_page, error_out=False)
 
-    colaborador = Colaborador.query.get_or_404(id_colaborador)
-    
-    return render_template('/tabelas/associacoes/tabela_inscricoes_por_colaborador.html', inscricoes=inscricoes, colaborador=colaborador)
+    return render_template('/tabelas/associacoes/tabela_inscricoes_por_colaborador.html', inscricoes=inscricoes, colaborador_nome=colaborador.nome)

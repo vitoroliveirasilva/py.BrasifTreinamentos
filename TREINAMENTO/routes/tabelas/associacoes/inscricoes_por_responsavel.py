525c4e10185824d00c21ab19c0cfec2718a7ddf2
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-10 14:42:10 -0300

    FIX - Inscrições por responsável

--- TREINAMENTO/routes/tabelas/associacoes/inscricoes_por_responsavel.py
+++ TREINAMENTO/routes/tabelas/associacoes/inscricoes_por_responsavel.py
@@ -1,23 +1,19 @@
 from flask import render_template, request
-from TREINAMENTO import db
 from flask_login import login_required
-from TREINAMENTO.models import Inscricao, Treinamento, Colaborador, Marca, Responsavel
+from TREINAMENTO.models import Inscricao, Responsavel
 from .. import tabela_bp
 
 @tabela_bp.route('/inscricoes/responsavel/<int:id_responsavel>')
 @login_required
 def inscricoes_por_responsavel(id_responsavel):
+    # Busca o responsável pelo ID, ou retorna 404 se não encontrar
+    responsavel = Responsavel.query.get_or_404(id_responsavel)
+
+    # Parâmetros de paginação
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
 
-    # Consulta para trazer as inscrições feitas por um responsável específico, incluindo o colaborador
-    inscricoes = db.session.query(Inscricao, Treinamento, Marca, Colaborador)\
-        .join(Treinamento, Inscricao.id_treinamento == Treinamento.id_treinamento)\
-        .join(Marca, Treinamento.id_marca == Marca.id_marca)\
-        .join(Colaborador, Inscricao.id_colaborador == Colaborador.id_colaborador)\
-        .filter(Inscricao.id_responsavel == id_responsavel)\
-        .paginate(page=page, per_page=per_page)
+    # Faz a consulta de todas as inscrições vinculadas ao responsável, sem filtrar por status
+    inscricoes = Inscricao.query.filter_by(id_responsavel=id_responsavel).paginate(page=page, per_page=per_page)
 
-    responsavel = Responsavel.query.get_or_404(id_responsavel)
-
-    return render_template('/tabelas/associacoes/tabela_inscricoes_por_responsavel.html', inscricoes=inscricoes, responsavel=responsavel)
+    return render_template('/tabelas/associacoes/tabela_inscricoes_por_responsavel.html', inscricoes=inscricoes, responsavel_name=responsavel.nome)

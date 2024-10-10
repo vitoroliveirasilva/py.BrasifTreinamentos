9f1f8e5d611752d09ddf84ffbabc1b42004469bf
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-10 12:05:38 -0300

    FIX - Colaboradores por responsavel

--- TREINAMENTO/routes/tabelas/associacoes/colaboradores_por_responsavel.py
+++ TREINAMENTO/routes/tabelas/associacoes/colaboradores_por_responsavel.py
@@ -1,20 +1,19 @@
 from flask import render_template, request
-from flask_login import login_required
-from TREINAMENTO import db
-from TREINAMENTO.models import Colaborador, Responsavel
+from flask_login import login_required, current_user
+from TREINAMENTO.models import Colaborador
 from .. import tabela_bp
 
 @tabela_bp.route('/colaboradores/responsavel/<int:id_responsavel>')
 @login_required
 def colaboradores_por_responsavel(id_responsavel):
+    # Parâmetros de paginação
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
+    
+    # Consulta para trazer todos os colaboradores por responsável (ativos e inativos)
+    colaboradores = Colaborador.query.filter_by(id_responsavel=id_responsavel).paginate(page=page, per_page=per_page, error_out=False)
 
-    # Consulta para trazer os colaboradores vinculados a um responsável específico
-    colaboradores = db.session.query(Colaborador)\
-        .filter(Colaborador.id_responsavel == id_responsavel)\
-        .paginate(page=page, per_page=per_page)
+    # Nome do responsável
+    responsavel_name = current_user.nome
 
-    responsavel = Responsavel.query.get_or_404(id_responsavel)
-
-    return render_template('tabelas/associacoes/tabela_colaboradores_por_responsavel.html', colaboradores=colaboradores, responsavel=responsavel)
+    return render_template('tabelas/associacoes/tabela_colaboradores_por_responsavel.html', colaboradores=colaboradores, responsavel_name=responsavel_name)

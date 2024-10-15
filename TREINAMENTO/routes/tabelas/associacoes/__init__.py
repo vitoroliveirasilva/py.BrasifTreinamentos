5384fadabe9c3379786c03fa6fe4499ccdb9baa0
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-14 22:58:59 -0300

    FIX - Atualização do INIT em associacoes

--- TREINAMENTO/routes/tabelas/associacoes/__init__.py
+++ TREINAMENTO/routes/tabelas/associacoes/__init__.py
@@ -1,11 +1,12 @@
-from .logins_por_colaborador import *
-from .logins_por_marca import *
+from .logins_por_colaborador import * #
 
-from .inscricoes_por_colaborador import *
-from .inscricoes_por_responsavel import *
+from .inscricoes_por_colaborador import * #
+from .inscricoes_por_responsavel import * #
 from .inscricoes_por_treinamento import *
 
-from .colaboradores_por_empresa import *
-from .colaboradores_por_responsavel import *
+from .colaboradores_por_empresa import * #
+from .colaboradores_por_responsavel import * #
 
 from .treinamentos_por_tipo import *
+from .logins_por_marca_tipo import * #
+from .treinamentos_por_marca_tipo import * #
